c3a152a6acb8737ab8e7d7788c5d6ba96e36ed4a
Alex Kit <source.kit@gmail.com>
2018-02-25 22:17:23 +0100

    v0.5.2

--- src/util/log.ts
+++
@@ -1,4 +0,0 @@
-export function log_error(...args) {
-	args.unshift('<i18n>');
-	console.log.apply(console, args);
-};
--- src/util/rgx.ts
+++
@@ -1,8 +0,0 @@
-export function rgx_find(str: string, rgx: RegExp, groupNumber: number) {
-	var match = rgx.exec(str);
-
-	if (match && match[groupNumber])
-		return match[groupNumber];
-
-	return null;
-}
