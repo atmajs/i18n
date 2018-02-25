78c6ef53779e8e2b7e43258cf33e0a87050edaed
Alex Kit <source.kit@gmail.com>
2018-02-25 23:12:35 +0100

    v0.5.6

--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'io',
-	main: './ts-temp/export.d.ts',
-	out: './typings/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/typings/index.d.ts', './lib/localization.d.ts');
